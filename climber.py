303a3454cbc45e2045557192cb08acea3f8c7de2
The Most Boring Person In The World Patryk Zmyslony <selppkez@gmail.com>
2020-03-05 13:47:40 +0000

    goofy slimeblal x

--- climber.py
+++ climber.py
@@ -16,8 +16,7 @@ class Climber(pygame.sprite.Sprite):
         """
         super().__init__()
         self.image = pygame.Surface(size)
-        self.image.fill((255, 9, 255))
-        self.image.set_colorkey((255, 9, 255))
+        self.image = pygame.image.load("standby.png")
         self.collision = [False]
         pygame.draw.rect(self.image, color, [0, 0, size[0], size[1]])
         self.rect = self.image.get_rect()
@@ -26,4 +25,22 @@ class Climber(pygame.sprite.Sprite):
         self.rect.x = 0
         self.rotation = 0
         self.bounce = 0
-
+        self.velocity = 5
+    
+    def update(self):
+        self.rect.y += self.velocity
+        if self.rect.y > 615:
+            self.rect.y = 615
+            
+    def jump(self):
+        if self.rect.y == 615:
+            self.rect.y -= self.velocity
+            
+    def moveRight(self):
+        self.rect.x += self.velocity
+        if self.rect.x > 615:
+            self.rect.x = 615        
+    def moveLeft(self):
+        self.rect.x -= self.velocity
+        if self.rect.x < 0:
+            self.rect.x = 0
--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def main():
     size = (700, 700)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Climber 1.0")
-    climberactor = Climber((255, 255, 255), (10, 10), (350, 350))
+    climberactor = Climber((255, 255, 255), (0, 0), (350, 350))
     carry = True
     all_sprites_list = pygame.sprite.Group()
     all_sprites_list.add(climberactor)
     clock = pygame.time.Clock()
-
+    x = 50
+    y = 50
+    width = 40
+    height = 60
+    vel = 5
+    
     while carry:
         screen.fill((0, 0, 0))
         all_sprites_list.draw(screen)
@@ -30,11 +35,18 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
                     carry = False
-            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    climberactor.moveRight()
+                if event.key == pygame.K_LEFT:
+                    climberactor.moveLeft()
                 if event.key == pygame.K_SPACE:
-                    print("space up")
+                    climberactor.jump()
+
+            
+            
+        climberactor.update()    
         font = pygame.font.Font(None, 50)
-        text = font.render(str('Hello world'), 255, (255, 255, 0))
+        text = font.render(str(climberactor.rect.y), 255, (255, 255, 0))
         screen.blit(text, (355, 10))
         pygame.display.flip()
         clock.tick(60)
